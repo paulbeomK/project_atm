8ea986ffb10d950c76eb1cbbb420860cc8f227de
paulbk <qjarb3411@gmail.com>
2024-11-28 22:53:36 +0900

    🎉 init

---
+++ Account.h
@@ -0,0 +1,8 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <string>
+
+class Account{
+
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Account.h"
+
+int main(){
+    // TODO
+    // make bank account 
+    // functionalities
+    // Insert Card => PIN number => Select Account => See Balance/Deposit/Withdraw
+    return 0;
+}
